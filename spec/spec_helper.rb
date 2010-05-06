ce07906cbbdd5eecc6aabd293017cc474a775684
Klaas Jan Wierenga <k.j.wierenga@gmail.com>
2010-05-06 11:21:02 +0200

    Discontinue use of Spork.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,24 +1,11 @@
 require 'rubygems'
-require 'spork'
 require 'right_aws' # load RightHttpConnection before FakeWeb otherwise we get buggy
 require 'fakeweb'
 
-Spork.prefork do
+require 'spec/autorun'
 
-  require 'spec/autorun'
-
-  Spec::Runner.configure do |config|
-    # == Notes
-    #
-    # For more information take a look at Spec::Runner::Configuration and Spec::Runner
-  end
-end
-
-Spork.each_run do
-
-  Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f }
-
-  Spec::Runner.configure do |config|
-    # config.before(:each) { Sham.reset }
-  end
+Spec::Runner.configure do |config|
+  # == Notes
+  #
+  # For more information take a look at Spec::Runner::Configuration and Spec::Runner
 end
