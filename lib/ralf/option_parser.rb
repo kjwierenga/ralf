4c774d728c69b24930204a4acd97838c5124a275
Klaas Jan Wierenga <k.j.wierenga@gmail.com>
2010-05-10 11:26:10 +0200

    Expand command-line documentation and update README.rdoc accordingly.

--- lib/ralf/option_parser.rb
+++ lib/ralf/option_parser.rb
@@ -7,42 +7,85 @@ class Ralf::OptionParser
     options = {}
 
     opts = ::OptionParser.new do |opts|
-      opts.banner = "Usage: #{$0} [options]
+      opts.banner = <<USAGE_END
+Usage: #{$0} [options]
 
-Download, merge and convert Amazon S3 log files for a specified date or date range.
+Download and merge Amazon S3 bucket log files for a specified date range and
+output a Common Log File. Ralf is an acronym for Retrieve Amazon Log Files.
 
-ralf reads options from '~/.ralf.yaml' or '/etc/ralf.yaml'. These files must be in YAML format.
+Ralf downloads bucket log files to local cache directories, merges the Amazon Log
+Files and converts them to Common Log Format.
 
-Example:
-  out_path:          /var/log/amazon_s3
-  log_file:          /var/log/ralf.log
-  aws_access_key_id: my_secret_key_id
+Example: #{$0} --range month --now yesterday --output-file '/var/log/amazon/:year/:month/:bucket.log'
 
-Command line options override the options loaded from the configuration file."
+AWS credentials (Access Key Id and Secret Access Key) are required to access
+S3 buckets. For security reasons these credentials can only be specified in a
+configuration file (see --config-file) or through the environment using the
+AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment variables.
+USAGE_END
 
       opts.separator ""
-      opts.separator "Input options:"
-      
-      opts.on("-b", "--buckets x,y,z", Array, "List of buckets for which to process logfiles. Optional, defaults to all buckets for user.") do |buckets|
+      opts.separator "Log selection options:"
+      opts.on("-l", "--[no-]list", "List buckets that have logging enabled. Does not process log files.") do |value|
+        options[:list] = value
+      end
+      opts.on("-b", "--buckets x,y,z", Array, "Buckets for which to process log files. Defaults to all log-enabled buckets.") do |buckets|
         options[:buckets] = buckets.compact
       end
-      opts.on("-r", "--range BEGIN[,END]", Array, "Date or date range to process. Optional, defaults to 'today'") do |range|
+      opts.on("-r", "--range BEGIN[,END]", Array, "Date or date range to process. Defaults to 'today'.") do |range|
         options[:range] = range.compact
       end
-      opts.on("-t", "--now TIME", "Date to use as base range off. Optional, defaults to 'today'") do |now|
+      log_selection_help =<<LOG_SELECTION_HELP
+Date to use as base for range. Defaults to 'today'.
+
+    You can use Chronic expressions for '--range' and '--now'. See http://chronic.rubyforge.org.
+    
+    Example: --range 'last week'
+      All days of previous week.
+    Example: --range 'this week'
+      Beginning of this week (sunday) upto and including today.
+    Example: --range '2010-01-01','2010-04-30'
+      First four months of this year.
+    Example: --range 'this month' --now yesterday
+      This will select log files from the beginning of yesterday's month upto and including yesterday.
+
+    The --buckets, --range and --now options are optional. If unspecified, (incomplete)
+    logging for today will be processed for all buckets (that have logging enabled).
+    This is equivalent to specifying "--range 'today'" and "--now 'today'".
+LOG_SELECTION_HELP
+      opts.on("-t", "--now TIME", log_selection_help) do |now|
         options[:now] = now
       end
-      opts.separator "You can use Chronic expressions for '--range' and '--now'. See http://chronic.rubyforge.org."
+      
+      # opts.on("-m", "--[no-]rename-bucket-keys", "Rename original log files on Amazon using format from '--cache-dir' option.") do |value|
+      #   options[:rename_bucket_keys] = value
+      # end
 
-      opts.separator ""
+      # opts.separator ""
       opts.separator "Output options:"
 
-      opts.on("-o", "--output-file FORMAT", "Output file format, e.g. '/var/log/s3/:year/:month/:bucket.log'") do |format|
-        options[:output_file_format] = format
+      output_file_help =<<OUTPUT_FILE_HELP
+Output file, e.g. '/var/log/s3/:year/:month/:bucket.log'. Required.
+
+    The --output-file format uses the last day of the range specified by (--range)
+    to determine the filename. E.g. when the format contains ':year/:month/:day' and
+    the range is 2010-01-15..2010-02-14, then the output file will be '2010/02/14'.
+OUTPUT_FILE_HELP
+      opts.on("-o", "--output-file FORMAT", output_file_help) do |format|
+        options[:output_file] = format
       end
       
-      opts.on("-x", "--cache-dir FORMAT", "Directories to cache downloaded log files, e.g. '/var/run/s3_cache/:year/:month/:day/:bucket'") do |format|
-        options[:cache_dir_format] = format
+      cache_dir_help =<<CACHE_DIR_HELP
+Directory name(s) in which to cache downloaded log files. Optional.
+
+    The --cache-dir format expands to as many directory names as needed for the 
+    range specified by --range. E.g. "/var/run/s3_cache/:year/:month/:day/:bucket"
+    expands to 31 directories for range 2010-01-01..2010-01-31.
+    
+    Defaults to '~/.ralf_cache/:bucket'.
+CACHE_DIR_HELP
+      opts.on("-x", "--cache-dir FORMAT", cache_dir_help) do |format|
+        options[:cache_dir] = format
       end
 
       # opts.on("-f", "--output-dir-format FORMAT", "Output directory format, e.g. ':year/:month/:day'") do |format|
@@ -57,25 +100,34 @@ Command line options override the options loaded from the configuration file."
       #   options[:output_prefix] = string
       # end
       
-      opts.on("-l", "--[no-]list", "List buckets that have logging enabled.") do |value|
-        options[:list] = value
+      # opts.separator ""
+      opts.separator "Config file options:"
+      config_file_help =<<CONFIG_FILE_HELP
+Path to file with configuration settings (in YAML format).
+
+    Configuration settings are read from the (-c) specified configuration file
+    or from ~/.ralf or from /etc/ralf (in that order, if the file exists).
+    Command-line options override settings read from a configuration file.
+
+    Configuration files must be in YAML format. Each command-line options has an
+    equivalent setting in a configuration file replacing dash (-) by underscore(_).
+
+    The Amazon Access Key Id and Secret Access Key can only be specified in the 
+
+    Example:
+      output_file:           /var/log/amazon_s3/:year:month/:bucket.log
+      aws_access_key_id:     my_access_key_id
+      aws_secret_access_key: my_secret_access_key
+
+    Command-line options always override the options loaded from the configuration file.
+CONFIG_FILE_HELP
+      opts.on("-c", "--config-file FILE", config_file_help) do |file|
+        options[:config_file] = file
       end
       
-
-      # opts.on("-m", "--[no-]rename-bucket-keys", "Rename original log files on Amazon using format from '--cache-dir' option.") do |value|
-      #   options[:rename_bucket_keys] = value
-      # end
-
-      opts.separator ""
       opts.separator "Debug options:"
       opts.on("-d", "--[no-]debug [aws]", "Show debug messages.") do |aws|
         options[:debug] = aws || true
-      end
-
-      opts.separator ""
-      opts.separator "Config file options:"
-      opts.on("-c", "--config-file FILE", "Path to configuration YAML file.") do |file|
-        options[:config_file] = file
       end
 
       opts.separator ""
