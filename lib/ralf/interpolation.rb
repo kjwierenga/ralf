10a91275ff02874a752659efcd99af37a648081c
Klaas Jan Wierenga <k.j.wierenga@gmail.com>
2010-05-10 17:14:54 +0200

    Add mandatory expansion of :bucket variable.

--- lib/ralf/interpolation.rb
+++ lib/ralf/interpolation.rb
@@ -2,21 +2,28 @@ class Ralf
 
   class Interpolation
     class NotAllInterpolationsSatisfied < StandardError ; end
+    class VariableMissing               < StandardError ; end
 
-    def self.interpolate(date, string)
-      processor = Ralf::Interpolation.new(date, string)
+    def self.interpolate(string, date, bucket = nil)
+      raise VariableMissing, ':bucket variable missing' unless bucket.nil? or string.match(/:bucket/)
+      processor = Ralf::Interpolation.new(string, date, bucket)
       raise NotAllInterpolationsSatisfied, "Not all keys are interpolated: '#{string}'" if processor.result.match(/:/)
       processor.result
     end
 
     attr :result
 
-    def initialize(date, string)
+    def initialize(string, date, bucket = nil)
+      @bucket = bucket
       @date = date
       @result = string.dup
       Ralf::Interpolation.instance_methods(false).each do |tag|
-        @result.gsub!(/:#{tag}/, self.send( tag ))
+        @result.gsub!(/:#{tag}/, self.send( tag )) unless self.send(tag).nil?
       end
+    end
+    
+    def bucket
+      @bucket
     end
 
     def week
--- spec/ralf/interpolation_spec.rb
+++ spec/ralf/interpolation_spec.rb
@@ -4,24 +4,42 @@ require 'ralf/interpolation'
 
 describe Ralf::Interpolation do
   before(:all) do
+    @bucket = 'bucket.mybuckets.org'
     @date = Date.strptime('2010-02-09')
   end
 
   [
-    [':year', '2010'],
+    [':year',   '2010'],
     [':month',  '02'],
     [':day',    '09'],
     [':week',   '06']
-  ].each do |match, result|
+  ].each do |match, result, bucket|
     it "should replace '#{match}' with '#{result}'" do
-      Ralf::Interpolation.interpolate(@date, match).should  eql(result)
+      Ralf::Interpolation.interpolate(match, @date).should  eql(result)
     end
   end
 
   it "should raise an error when not all symbols could be interpolated" do
     lambda {
-      Ralf::Interpolation.interpolate(@date, ':unknown')
-    }.should  raise_error(Ralf::Interpolation::NotAllInterpolationsSatisfied)
+      Ralf::Interpolation.interpolate(':unknown', @date)
+    }.should raise_error(Ralf::Interpolation::NotAllInterpolationsSatisfied)
+  end
+  
+  it "should interpolate :bucket" do
+    Ralf::Interpolation.interpolate(':year/:month/:day/:bucket.log',
+      @date, @bucket).should eql('2010/02/09/bucket.mybuckets.org.log')
+  end
+
+  it "should require :bucket interpolation if bucket specified" do
+    lambda {
+      Ralf::Interpolation.interpolate(':year/:month/:day.log', @date, @bucket)
+    }.should raise_error(Ralf::Interpolation::VariableMissing, ':bucket variable missing')
+  end
+  
+  it "should raise an error when :bucket can not be interpolated" do
+    lambda {
+      Ralf::Interpolation.interpolate(':bucket', @date)
+    }.should raise_error(Ralf::Interpolation::NotAllInterpolationsSatisfied)
   end
 
 end
