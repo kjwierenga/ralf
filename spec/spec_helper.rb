dc4148b9382d05321cf55ea062c995d735c42db8
Klaas Jan Wierenga <k.j.wierenga@gmail.com>
2010-05-17 17:36:40 +0200

    Helper method to load example buckets.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,4 +8,17 @@ Spec::Runner.configure do |config|
   # == Notes
   #
   # For more information take a look at Spec::Runner::Configuration and Spec::Runner
+  
+  def load_example_bucket_mocks
+    buckets = YAML.load(File.open(File.join(File.dirname(__FILE__), 'fixtures', 'example_buckets.yaml')))
+    buckets = buckets.inject({}) do |memo, info|
+      info[:keys].map! do |key|
+        mock(key[:name], key)
+      end
+      memo.merge!(info[:name] => mock(info[:name], info))
+      memo
+    end
+    buckets
+  end
 end
+
